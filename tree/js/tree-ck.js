ff5c3a06e9f0b8aed1ae84bb289ceb9714519c41
Martin Doudoroff <martin@doudoroff.com>
2012-08-13 16:12:35 -0400

    work

--- tree/js/tree-ck.js
+++ tree/js/tree-ck.js
@@ -9442,7 +9442,7 @@ function createBox(boxType,width,height,left,top) {
 }
 
 function createBoxesForElements(container,e1,elementArray) {
-	log(e1,elementArray);
+	//log(e1,elementArray);
 	var e1pos = e1.position();
 	var e1height = e1.outerHeight(false);
 	
@@ -9458,14 +9458,44 @@ function createBoxesForElements(container,e1,elementArray) {
 		e2height = $(this).outerHeight(false);
 		
 		w = e2pos.left - e1pos.left - 18;
-		h = e2pos.top - e1pos.top - e1height * 1.5 + e2height;
+		h = e2pos.top - e1pos.top - e1height * 1.5 + e2height - 2;
 		x = e1pos.left + 9;
 		y = e1pos.top + e1height * 1.5;
 		container.append(createBox('el',w,h,x,y));
 	});
 }
 
+function createSubTreeBox(container,e1,elementArray) {
+	var e1pos = e1.position();
+	var e1height = e1.outerHeight(false);
+	
+	var e2pos;
+	var e2height;
+	
+	var w;
+	var h;
+	var x;
+	var y;
+		
+	w = container.width() - e1.width() - 50;
+	h = (e1pos.top+e1height/2 - 23)/2;
+		
+	x = e1.width() + 8;
+	y = e1pos.top+e1height/2-h;
+		
+	container.append(createBox('zeeBottom',w,h,x,y));
+
+	w = 70;
+	x = (container.width()-w)+28;
+	y = 27;
+		
+	container.append(createBox('zeeTop',w,h,x,y));
+		
+}
+
+
 function rebuildHierarchyBoxes() {
+
 	var section;
 	var source;
 	var sourceUL;
@@ -9473,31 +9503,59 @@ function rebuildHierarchyBoxes() {
 
 	$('section').each( function() {
 
+		// strip out all the "lines"
 		$('.el').remove();
 
+		
+		
 		section = $(this);
 		
-		// handle the root special case
+		// handle the special case connecting the h1 to the first children
+		// @@ THIS CODE (the while loop in particular) IS HORSESHIT----REWORK
 		source = $(this).find('h1')[0];
 		sourceUL = $(source).next();
+		var i = 0;
+		while (!sourceUL.is('ul')) {
+			sourceUL = $(sourceUL).next();
+			if (sourceUL === undefined) {
+				break;
+			}
+			i = i + 1;
+			if (i > 1000) {
+				break;
+			}
+		}
 		if (sourceUL !== undefined) {
-			destinations = $(sourceUL).children('.abstract, .generic, .weakcat, .adhoc');
+			destinations = $(sourceUL).children('li');
 			createBoxesForElements( section, $(source), destinations );
 		}
 		
-		
-		$(this).find('.abstract, .generic, .weakcat, .adhoc').each( function() {
-			source = $(this);
-			sourceUL = source.next();
-			log( sourceUL);
-			if (sourceUL !== undefined) {
-				destinations = $(sourceUL).children('.abstract, .generic, .weakcat, .adhoc');
-				createBoxesForElements( $(this), $(source), destinations );
-			}
+		// add the lines for the regular parent/child relationships
+		var parent;
+		$(this).find('.nonproduct').each( function() {
+
+			parent = $(this);
+			$(section).find('ul[data-par=' + parent.attr('id') + ']').children('li').each( function() {
+				createBoxesForElements( section, $(parent), $(this) );
+			});
+
 		});
+
 		
 		
 	});
+	
+}
+
+function rebuildSubTreeBox() {
+	
+	$('.zee').remove();
+	
+	var section = $('nav')[0];
+	var source = $('#selectedSubTree')[0];
+	var destinations = $('#sectionHeading');
+		
+	createSubTreeBox( $(section), $(source), destinations );
 	
 }
 
@@ -9506,15 +9564,6 @@ jQuery(document).ready(function() {
 	var filters = $("nav");
 	var button;
 
-	// == add a "show all" un-filter option
-	/*
-	button = $('<div class="toggle">Show All</div>');
-	button.click( function() {
-		$('#treeContainer').find().show(500);
-	});
-	filters.append(button);
-	*/
-	
 	// == spacer
 	filters.append('<br>');
 	
@@ -9535,37 +9584,15 @@ jQuery(document).ready(function() {
 	// == spacer
 	filters.append('<br>');
 
-	// == build some section filters
-	/*
-	var headings;
-	var heading;
-	var sid;
-	$('#treeContainer').children("section").each( function() {
 
-		sid = $(this).attr('id');
-		
-		button = $('<div class="toggle"></div>');
-		button.data("sectionID",sid);
-		
-		headings = $(this).find('h1');
-		headings.each( function() {
-			heading = $(this)[0];
-			button.text( $(heading).text() );
-		});
-
-		// bind
-		button.click( function() {
-			$('#treeContainer').find('section').hide();
-			$('#'+$(this).data('sectionID')).show();
-		});
-		
-		filters.append(button);
-	});
-	*/
-
-	//$('#treeContainer').append( createBox('el',150,150,50,50) );
-
+	// == hack in the "lines"
+	
 	rebuildHierarchyBoxes();
+
+	var selected = $('#selectedSubTree');
+	if (selected.length) {
+		rebuildSubTreeBox();
+	}
 
 });
 
