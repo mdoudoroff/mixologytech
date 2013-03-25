f5038053fb86a4669e7c2b99ba9f77fb4c8490f1
Martin Doudoroff <martin@doudoroff.com>
2013-03-24 22:16:57 -0400

    site work

---
+++ mybar2/js/mybar-ck.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){$.getJSON("/aggregates.json",function(e){$(".AgTotalEntries").text(e.totalEntries);$(".AgAvgNewEntries").text(e.averageNewEntries);$(".AgTotalBarcodes").text(e.grandTotalUPCs);$(".AgAvgNewBarcodes").text(e.averageNewBarcodes)})});
---
+++ mybar2/js/mybar.js
@@ -0,0 +1,20 @@
+
+jQuery(document).ready(function() {
+
+	// fill in some stuff
+	$.getJSON('/aggregates.json', function(data) {
+
+		$('.AgTotalEntries').text(data.totalEntries);
+
+		$('.AgAvgNewEntries').text(data.averageNewEntries);
+
+		$('.AgTotalBarcodes').text(data.grandTotalUPCs);
+
+		$('.AgAvgNewBarcodes').text(data.averageNewBarcodes);
+
+	});
+
+
+
+
+});
