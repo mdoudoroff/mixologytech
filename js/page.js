47646164ac7e291d9bab0e28cafb551611538856
Martin Doudoroff <martin@doudoroff.com>
2016-09-18 11:54:02 -0400

    updates

--- js/page.js
+++ js/page.js
@@ -3,6 +3,12 @@ var appsToggleState = 'closed';
 
 jQuery(document).ready(function() {
 
+    $('#syncgridtop').hide();
+    setInterval(function() { 
+        $('#syncgridtop').fadeToggle(1000);
+    }, 3000);
+
+
 	// global nav stuff
 
 	$('#menuToggle').click(function(event) {
